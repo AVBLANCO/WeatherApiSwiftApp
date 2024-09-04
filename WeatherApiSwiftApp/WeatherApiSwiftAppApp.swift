7ad9fbd879271187dd2711261d77262a63a42653
Victor Manuel Blanco Mancera <victor.blanco@ext.etniabarcelona.com>
2024-09-04 03:26:21 -0500

    Initial Commit

---
+++ WeatherApiSwiftApp/WeatherApiSwiftAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApiSwiftAppApp.swift
+//  WeatherApiSwiftApp
+//
+//  Created by Victor Manuel Blanco Mancera on 4/09/24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApiSwiftAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
